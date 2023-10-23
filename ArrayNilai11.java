98694b05ac4c18c67ad11c0dedb354f80f855584
FarhanMawaludin <farhanmawaludin7@gmail.com>
2023-10-23 12:27:43 +0700

    perubahan

--- ArrayNilai11.java
+++ ArrayNilai11.java
@@ -3,14 +3,17 @@ public class ArrayNilai11 {
     public static void main(String[] args) {
         Scanner input11 = new Scanner(System.in);
         int[] nilaiAkhir = new int [10];
-        for (int i = 0; i < 10 ; i++){
+        for (int i = 0; i < nilaiAkhir.length; i++){
             System.out.print("Masukan Nilai Akhir ke-" + i + " : ");
             nilaiAkhir[i] =  input11.nextInt();
         }
 
-        for (int i = 0;
-        i <10; i++){
-            System.out.println("Nilai akhir ke-" + i + "adalah" + nilaiAkhir[i]);
+        for (int i = 0; i <nilaiAkhir.length; i++){
+            if (nilaiAkhir[i] > 70){
+            System.out.println("Mahasiswa ke-" + i + "Lulus");
+        }else {
+            System.out.println("Mahasiswa ke-" + i + "Tidak lulus");
+        }
         }
     }
 }
